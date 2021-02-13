0a2fa967e454ceb55a801ea22f8197a0ccbc3f94
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-02-13 13:04:12 +0200

    change connections

--- config/database.js
+++ config/database.js
@@ -53,19 +53,19 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: "mysql",
-    connection: {
-      host: Env.get("DB_HOST", "localhost"),
-      port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", "root"),
-      password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "adonis"),
-    },
-    debug: Env.get("DB_DEBUG", false),
-  },
+  // mysql: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: Env.get("DB_HOST", "localhost"),
+  //     port: Env.get("DB_PORT", ""),
+  //     user: Env.get("DB_USER", "root"),
+  //     password: Env.get("DB_PASSWORD", ""),
+  //     database: Env.get("DB_DATABASE", "adonis"),
+  //   },
+  //   debug: Env.get("DB_DEBUG", false),
+  // },
 
-  connection: {
+  mysql: {
     host: Env.get("DB_HOST", CLEARDB_DATABASE_URL.host),
     port: Env.get("DB_PORT", ""),
     user: Env.get("DB_USER", CLEARDB_DATABASE_URL.username),
