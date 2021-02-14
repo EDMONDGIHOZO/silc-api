e93fbfe2df636308222fa0e3c54e0ba9a42f026f
EDMONDGIHOZO <gihozoedmond@gmail.com>
2021-02-14 21:56:21 +0200

    changing the origin issue

--- config/cors.js
+++ config/cors.js
@@ -16,9 +16,11 @@ module.exports = {
     | Function - Receives the current origin and should return one of the above values.
     |
     */
-    origin: function(currentOrigin) {
-        return currentOrigin === "http://localhost:8080", "https://silc.vercel.app";
-    },
+    // origin: function(currentOrigin) {
+    //     return currentOrigin === "http://localhost:8080", "https://silc.vercel.app";
+    // },
+
+    origin: ['https://silc.vercel.app'],
 
     /*
     |--------------------------------------------------------------------------
